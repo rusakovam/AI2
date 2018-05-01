06aa0aeae4d143fc8ee159cf76a20443007761df
rusakovam <marina.rusakova@gmail.com>
2018-05-01 14:53:28 +0300

    fixed get_vectors

--- hometasks4/src/get_vectors.py
+++ hometasks4/src/get_vectors.py
@@ -12,18 +12,23 @@ import numpy as np
 from keras.applications.resnet50 import ResNet50
 from scipy.misc import imread, imresize
 
-file_names = os.listdir('dataset/')
+datasetDir = '../dataset/'
+vectorsDir = '../vectors/'
 
 model = ResNet50(include_top=False, pooling='avg')
 
 batch_size = 16
 
+if not os._exists(vectorsDir):
+    os.makedirs(vectorsDir)
+
+file_names = os.listdir(datasetDir)
 for i in range(0, len(file_names), batch_size):
     batch = file_names[i: i + batch_size]
     x_batch = np.zeros((len(batch), 224, 224, 3), dtype='float')
 
     for j, fn in enumerate(batch):
-        img = imread('dataset/' + fn, mode='RGB')
+        img = imread(datasetDir + fn, mode='RGB')
         img = imresize(img, (224, 224, 3))
         x_batch[j] = img
 
@@ -32,4 +37,4 @@ for i in range(0, len(file_names), batch_size):
     prediction = model.predict(x_batch)
 
     for j, fn in enumerate(batch):
-        np.save('vectors/' + fn + '.npy', prediction[j])
+        np.save(vectorsDir + fn + '.npy', prediction[j])
